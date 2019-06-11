0712557b1cb8d203ecd09ff0f05e6fb76f27c74a
Celia <celiafrom12@gmail.com>
2019-06-11 14:49:42 +0100

    defined a route and used shotgun

--- app.rb
+++ app.rb
@@ -1,1 +1,21 @@
 require 'sinatra'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "Secret page"
+end
+
+get '/home' do
+  "Homepage"
+end
+
+get '/file' do
+  "File"
+end
+
+get '/hey' do
+  "hey"
+end 
